472a26c2b8aba652711acb2b8e28a5c98268e212
jkddt <support@jkddt.com>
2017-04-08 20:15:02 +0800

    edit

--- pages/item/item.js
+++ pages/item/item.js
@@ -1,20 +1,28 @@
 // pages/item/item.js
 Page({
   data: {
-    showModalStatus: false,
-    animationData:{},
-    countData:1000,
-    examData:[],
+    examData: [],
+    isShow: false
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    console.log(options)
+    //console.log(options)
+    for (var i = 1; i <= 100; i++) {
+      this.data.examData.push({
+        content: i,
+        isClick: false
+      });
+    }
+    this.setData({ examData: this.data.examData })
   },
   onReady: function () {
     // 页面渲染完成
   },
   onShow: function () {
     // 页面显示
+    console.log(this.data.isShow)
+    this.setData({ isShow: !this.data.isShow })
+    console.log(this.data.isShow)
   },
   onHide: function () {
     // 页面隐藏
@@ -22,49 +30,18 @@ Page({
   onUnload: function () {
     // 页面关闭
   },
-  showModal: function () {
-    // 显示遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
+  itemClick: function (e) {
+    var index = e.currentTarget.dataset.index;
+    //console.log(e);
+    //this.data.examData[index].isClick = true;
+   // this.setData({ examData: this.data.examData })
+    var that = this;
+    this.data.examData.forEach(function(v,i){
+     if (i===index){
+       v.isClick=true;
+     }
     })
-    this.animation = animation
-    animation.translateY(300).step()
-    var examData=[];
-    for (var i=0;i<this.data.countData;i++){
-      examData.push(i);
-    }
-    this.setData({
-      animationData: animation.export(),
-      showModalStatus: true,
-      examData:examData      
-    }),
-    setTimeout(function () {
-      animation.translateY(0).step()
-      this.setData({
-        animationData: animation.export()
-      })
-    }.bind(this), 200)
-  },
-  hideModal: function () {
-    // 隐藏遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
-    })
-    this.animation = animation
-    animation.translateY(300).step()
-    this.setData({
-      animationData: animation.export(),
-    })
-    setTimeout(function () {
-      animation.translateY(0).step()
-      this.setData({
-        animationData: animation.export(),
-        showModalStatus: false
-      })
-    }.bind(this), 200)
+    this.setData({ examData: this.data.examData })
   }
+
 })
