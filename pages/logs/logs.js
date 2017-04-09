54aa8c1ff88608ab3046b9afeb49c87e3b4b8e85
jkddt <support@jkddt.com>
2017-04-09 14:14:49 +0800

    edit

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -14,34 +14,39 @@ Page({
     })
   },
   touchstart: function (e) {
+    var changedTouches = e.changedTouches[0]
     this.data.logs.forEach(function (v, i) {
       if (v.isTouchMove) {
         v.isTouchMove = false;
       }
     })
-    this.setData({ logs: this.data.logs, startX: e.changedTouches[0].clientX, startY: e.changedTouches[0].clientY })
-    // console.log(e)
+    this.setData({ logs: this.data.logs, startX: changedTouches.clientX, startY: changedTouches.clientY })
   },
   touchmove: function (e) {
-    var that = this
     var index = e.currentTarget.dataset.index
     var touchMoveX = e.changedTouches[0].clientX
     var touchMoveY = e.changedTouches[0].clientY
-    var angle = this.angle({ X: this.data.startX, Y: this.data.startY }, { X: touchMoveX, Y: touchMoveY })
+    var startX = this.data.startX
+    var startY = this.data.startY
+    //var angle = util.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY })
+    var angle = util.getSlideDirection({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY })
     this.data.logs.forEach(function (v, i) {
       v.isTouchMove = false
-      if (Math.abs(angle) > 30) {
+      // if (Math.abs(angle) > 30) {
+      //   return;
+      // }
+      if (angle < 3) {
         return;
       }
       if (i === index) {
-        if (touchMoveX > that.data.startX) {
+        if (touchMoveX > startX) {
           v.isTouchMove = false;
         } else {
           v.isTouchMove = true;
         }
       }
     })
-    this.setData({ logs: this.data.logs })   
+    this.setData({ logs: this.data.logs })
   },
   del: function (e) {
     var index = e.currentTarget.dataset.index
@@ -49,18 +54,10 @@ Page({
     this.setData({ logs: this.data.logs })
     wx.setStorage({
       key: "logs",
-      data: this.data.logs
+      data: (this.data.logs || []).map(function (log) {
+        return (new Date(log.content).getTime())
+      })
     })
-  },
-  /**
-   * 计算滑动角度
-   * @param {Object} start 起点坐标
-   * @param {Object} end 终点坐标
-   */
-  angle: function (start, end) {
-    var _X = end.X - start.X,
-      _Y = end.Y - start.Y
-    //返回角度 /Math.atan()返回数字的反正切值
-    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-  },
+  }
+
 })
--- utils/util.js
+++ utils/util.js
@@ -20,7 +20,52 @@ function getAppTitle() {
   return getApp().globalData.appTitle
 }
 
+//根据起点和终点返回方向 1：向上，2：向下，3：向左，4：向右,0：未滑动
+function getSlideDirection(start, end) {
+  var dy = start.Y - end.Y;
+  var dx = end.X - start.X;
+  var result = 0;
+  //如果滑动距离太短
+  if (Math.abs(dx) < 2 && Math.abs(dy) < 2) {
+    //未滑动
+    return result;
+  }
+  //返回滑动的角度
+  var angle = Math.atan2(dy, dx) * 180 / Math.PI;
+  if (angle >= -45 && angle < 45) {
+    //向右
+    result = 4;
+  }
+  else if (angle >= 45 && angle < 135) {
+    //向上
+    result = 1;
+  }
+  else if (angle >= -135 && angle < -45) {
+    //向下
+    result = 2;
+  }
+  else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+    //向左
+    result = 3;
+  }
+  return result;
+}
+
+/**
+  * 计算滑动角度
+  * @param {Object} start 起点坐标
+  * @param {Object} end 终点坐标
+  */
+function angle(start, end) {
+  var _X = end.X - start.X,
+    _Y = end.Y - start.Y
+  //返回角度 /Math.atan()返回数字的反正切值
+  return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+}
+
 module.exports = {
   formatTime: formatTime,
-  getAppTitle: getAppTitle
+  getAppTitle: getAppTitle,
+  getSlideDirection: getSlideDirection,
+  angle: angle
 }
