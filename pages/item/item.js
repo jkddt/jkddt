beb3df7c8ced58a0efadfea612db1a6fbcedce77
jkddt <support@jkddt.com>
2017-04-09 13:43:14 +0800

    edit

--- pages/item/item.js
+++ pages/item/item.js
@@ -34,14 +34,59 @@ Page({
     var index = e.currentTarget.dataset.index;
     //console.log(e);
     //this.data.examData[index].isClick = true;
-   // this.setData({ examData: this.data.examData })
+    // this.setData({ examData: this.data.examData })
     var that = this;
-    this.data.examData.forEach(function(v,i){
-     if (i===index){
-       v.isClick=true;
-     }
+    this.data.examData.forEach(function (v, i) {
+      if (i === index) {
+        v.isClick = true;
+      }
     })
-    this.setData({ examData: this.data.examData })
+    //渲染页面
+    that.setData({ examData: that.data.examData })
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '这是一个模态弹窗',
+    //   success: function (res) {
+    //     if (res.confirm) {
+    //       that.data.examData.forEach(function (v, i) {
+    //         if (i === index) {
+    //           v.isClick = true;
+    //         }
+    //       })
+    //       //渲染页面
+    //       that.setData({ examData: that.data.examData })
+    //     } else if (res.cancel) {
+    //       console.log('用户点击取消')
+    //     }
+    //   }
+    // })
+
+    // wx.showActionSheet({
+    //   itemList: ['A', 'B', 'C'],
+    //   success: function (res) {
+    //     console.log(res.tapIndex)
+    //   },
+    //   fail: function (res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
+    // wx.setNavigationBarTitle({
+    //   title: '当前页面'
+    // })
+
+    // wx.chooseImage({
+    //   count: 1, // 默认9
+    //   sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    //   sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+    //   success: function (res) {
+    //     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+    //     var tempFilePaths = res.tempFilePaths
+    //   }
+    // })
+wx.makePhoneCall({
+  phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+})
+
   }
 
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,13 +2,65 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    logs: [],
+    startX: 0,
+    startY: 0
   },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
+        return ({ content: util.formatTime(new Date(log)), isTouchMove: false })
       })
     })
-  }
+  },
+  touchstart: function (e) {
+    this.data.logs.forEach(function (v, i) {
+      if (v.isTouchMove) {
+        v.isTouchMove = false;
+      }
+    })
+    this.setData({ logs: this.data.logs, startX: e.changedTouches[0].clientX, startY: e.changedTouches[0].clientY })
+    // console.log(e)
+  },
+  touchmove: function (e) {
+    var that = this
+    var index = e.currentTarget.dataset.index
+    var touchMoveX = e.changedTouches[0].clientX
+    var touchMoveY = e.changedTouches[0].clientY
+    var angle = this.angle({ X: this.data.startX, Y: this.data.startY }, { X: touchMoveX, Y: touchMoveY })
+    this.data.logs.forEach(function (v, i) {
+      v.isTouchMove = false
+      if (Math.abs(angle) > 30) {
+        return;
+      }
+      if (i === index) {
+        if (touchMoveX > that.data.startX) {
+          v.isTouchMove = false;
+        } else {
+          v.isTouchMove = true;
+        }
+      }
+    })
+    this.setData({ logs: this.data.logs })   
+  },
+  del: function (e) {
+    var index = e.currentTarget.dataset.index
+    this.data.logs.splice(index, 1)
+    this.setData({ logs: this.data.logs })
+    wx.setStorage({
+      key: "logs",
+      data: this.data.logs
+    })
+  },
+  /**
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
+   */
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
 })
